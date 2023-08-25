9f246c4f1b16ba1c7824dad7517ace6b76b9c441
ippei0110 <ippei.0110.s@gmail.com>
2023-08-25 10:46:01 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+// import classes from "./Footer.module.css";
+// import Image from "next/image";
+import Link from "next/link";
+import classes from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <p className={classes.anchor}>Index</p>
+      </Link>
+      <Link href="/about">
+        <p className={classes.anchor}>About</p>
+      </Link>
+    </header>
+  );
+}
