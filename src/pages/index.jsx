cf26cad464a6cf485151f2b7d84df2e433c32cc4
ippei0110 <ippei.0110.s@gmail.com>
2023-08-26 21:32:12 +0900

    🎨 Custom Hooksを使ってまとめました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,52 +3,14 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (prevCount < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します。");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    console.log(`マウント時: ${count}`);
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      console.log(`アンマウント時: ${count}`);
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-  console.log(text);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={classes.container}>
@@ -57,10 +19,9 @@ export default function Home() {
       </Head>
       <Header />
       {isShow ? <h1>{count}</h1> : null}
-      <button href="/about" onClick={handleClick}>
-        ボタン
-      </button>
+      <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
       <ul>
